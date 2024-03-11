9077c038d70258e91d261b421ae413475411c2af
JGilbert <j.gilbert@mail.utoronto.ca>
2024-03-11 16:34:40 -0400

    cleans datasets
    creates 3 clean datasets, one for count by year, one for count by cause (only top 10 rank per year) and one including both

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,36 +9,48 @@
 
 #### Workspace setup ####
 library(tidyverse)
-
+library(janitor)
+library(dplyr)
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_parquet("data/raw_data/raw_mortality_data.parquet")
+head(cleaned_data)
 
 cleaned_data <-
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  janitor::clean_names() %>%
+  rename("year" = ref_date,
+         "cause" = leading_causes_of_death_icd_10,
+         "age" = age_at_time_of_death) %>%
+  mutate(value = ifelse(is.na(value), 0, value))
+
+colnames(cleaned_data)
+filtered_data <- cleaned_data %>%
+  filter(age == "Age at time of death, all ages" &
+         characteristics == "Number of deaths" &
+         sex == "Both sexes" &
+         uom == "Number")
+
+# Total mortaliies per year
+total_only <- filtered_data |>
+  select(year, cause, value) %>%
+  filter(cause == "Total, all causes of death [A00-Y89]") %>%
+  select(-cause) %>%
+  rename("total_deaths" = value)
+
+# Seperating by cause of death
+by_cause <- filtered_data |>
+  select(year, cause, value) %>%
+  filter(cause != "Total, all causes of death [A00-Y89]") 
+
+# Restricts to only top 10 causes
+by_cause <- by_cause %>%
+  group_by(year) %>%
+  mutate(year_rank = rank(-value)) %>%
+  filter(year_rank <= 10)
+
+# Add column for total deaths grouped by year
+joint_data <- 
+  left_join(by_cause, total_only, by = "year", relationship = "many-to-many")
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+
