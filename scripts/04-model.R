4b64d263e9bce2a7afc82bf8e33bccbe8ab106b0
Lwall02 <liam.wall@mail.utoronto.ca>
2024-03-16 21:49:29 -0400

    Updated model writng Quarto doc

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -44,10 +44,11 @@ joint_data |>
 ### Model data ####
 cause_of_death_ontario_neg_binomial <-
   stan_glm(
-    value ~ cause,
+    value ~ cause*year,
     data = joint_data,
     family = neg_binomial_2(link = "log"),
-    seed = 853
+    seed = 853,
+    cores = 2
   )
 
 summary(cause_of_death_ontario_neg_binomial)
@@ -63,6 +64,54 @@ ggplot(residuals_df, aes(x = residuals)) +
   geom_histogram(bins = 30, fill = "blue", color = "black") +
   theme_minimal() +
   labs(title = "Histogram of Residuals", x = "Residuals", y = "Count")
+
+mcmc_rhat(cause_of_death_ontario_neg_binomial)
+
+# Create Predictions for the next years
+recent_causes <- joint_data |>
+  filter(year == 2022)
+unique_causes <- distinct(recent_causes, cause)
+
+future_years <- rep(2023:2028, each = 10)
+future_years <- data.frame(year = 2023:2028)
+
+future_data <- expand.grid(year = future_years$year, cause = unique_causes$cause) |>
+  arrange(year)
+
+predicted_counts <- posterior_predict(cause_of_death_ontario_neg_binomial, newdata = future_data)
+mean_predicted_counts <- apply(predicted_counts, 2, mean)
+future_data$predicted_counts <- mean_predicted_counts
+
+future_data |>
+  arrange(cause)
+
+colnames(future_data)[3] <- "value"
+
+future_data |>
+  mutate(cause = str_remove(cause, "\\s*\\[.*\\]")) |>
+  ggplot(aes(x = year, y = value)) +
+  geom_point(aes(color = cause), alpha = 0.8) +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+joint_data |>
+  filter(cause == "COVID-19 [U07.1, U07.2, U10.9]")
+
+data_to_combine <- joint_data |>
+  select(-year_rank) |>
+  select(-total_deaths)
+
+combined_future_data <- rbind(data_to_combine, future_data)
+
+combined_future_data |>
+  tail()
+
+combined_future_data |>
+  mutate(cause = str_remove(cause, "\\s*\\[.*\\]")) |>
+  ggplot(aes(x = year, y = value)) +
+  geom_point(aes(color = cause), alpha = 0.8) +
+  theme_minimal() +
+  theme(legend.position = "bottom")
 
 #Can compare to fitting this data with a Poisson distribution 
 cause_of_death_ontario_poisson <-
@@ -87,8 +136,24 @@ ggplot(residuals_df, aes(x = residuals)) +
   theme_minimal() +
   labs(title = "Histogram of Residuals", x = "Residuals", y = "Count")
 
-
 #### Save model ####
-saveRDS(cause_of_death_ontario_neg_binomial, file = "models/cod_ontario_neg_binomial.rds")
+saveRDS(cause_of_death_ontario_neg_binomial, file = "models/cod_ontario_neg_binomial_two.rds")
 
 
+max(joint_data$total_deaths)
+
+
+
+joint_data |>
+  summarise(
+    min = min(value),
+    mean = mean(value),
+    max = max(value),
+    sd = sd(value),
+    var = sd^2,
+    n = n()
+    )
+
+joint_data |>
+  filter(cause == "Accidents (unintentional injuries) [V01-X59, Y85-Y86]") |>
+  arrange(-value)
